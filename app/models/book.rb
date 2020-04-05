e12c389d0c01430f620c030ba387a67ee5419586
Yusent Chig <yusent@protonmail.com>
2020-04-05 16:46:56 -0700

    Added search_by_title scope to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
+  scope :search_by_title, -> (title) do
+    words = title.split /\W+/
+    words.inject(all) do |results, word|
+      results.where "title like ?", "%#{word}%"
+    end
+  end
+
   validates_presence_of :title, :author, :isbn, :price, :short_description
   validates :isbn, isbn: true
 end
